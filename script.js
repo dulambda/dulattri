1fea982ff2fb841964f93d740a3208e879336083
Dulat M <dulat.mukhtarov.2004@gmail.com>
2025-10-09 13:56:24 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
             card.classList.add('fade-in-up');
         }, 200 * i);
     });
+    // Bounce animation for title only
+    const siteName = document.querySelector('.site-name');
+        if (siteName) {
+            siteName.classList.add('bounce');
+            setTimeout(() => {
+                siteName.classList.remove('bounce');
+            }, 2500);
+        }
 });
+function toggleMenu() {
+        const menu = document.getElementById('dropdownMenu');
+        if (menu.style.display === 'flex') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'flex';
+        }
+    }
+
+    // Закрывать меню при клике вне его
+    document.addEventListener('click', function(event) {
+        const menu = document.getElementById('dropdownMenu');
+        const icon = document.querySelector('.menu-icon');
+        if (!menu.contains(event.target) && !icon.contains(event.target)) {
+            menu.style.display = 'none';
+        }
+    });
